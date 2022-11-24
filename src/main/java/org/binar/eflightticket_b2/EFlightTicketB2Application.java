794bafdd5a3b66c7129172eab9b6c98bfda06ead
haris <haris.aulia@gmail.com>
2022-11-24 20:38:02 +0700

    initial commit

---
+++ src/main/java/org/binar/eflightticket_b2/EFlightTicketB2Application.java
@@ -0,0 +1,13 @@
+package org.binar.eflightticket_b2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFlightTicketB2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFlightTicketB2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/binar/eflightticket_b2/EFlightTicketB2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binar.eflightticket_b2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFlightTicketB2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
