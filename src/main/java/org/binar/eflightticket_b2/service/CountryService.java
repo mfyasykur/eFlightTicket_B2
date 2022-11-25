1ea7205668d03dcc3bb7fe1daa3b51cf5316ec94
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2022-11-25 19:46:17 +0700

    "Task 6,7,8,9 Country Entity"

--- src/main/java/org/binar/eflightticket_b2/service/CountryService.java
+++ src/main/java/org/binar/eflightticket_b2/service/CountryService.java
@@ -3,9 +3,16 @@ package org.binar.eflightticket_b2.service;
 import org.binar.eflightticket_b2.dto.CountryDTO;
 import org.binar.eflightticket_b2.entity.CountryEntity;
 
+import java.util.List;
+
 public interface CountryService {
 
     CountryEntity add(CountryEntity countryEntity);
+    CountryEntity update(Long id, CountryEntity countryEntity);
+    Boolean delete(Long id);
+    List<CountryEntity> findAll();
+    CountryEntity findById(Long id);
+
     
     CountryDTO mapToDto(CountryEntity countryEntity);
     CountryEntity mapToEntity(CountryDTO countryDTO);
--- src/main/java/org/binar/eflightticket_b2/service/impl/CountryServiceImpl.java
+++ src/main/java/org/binar/eflightticket_b2/service/impl/CountryServiceImpl.java
@@ -9,17 +9,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class CountryServiceImpl implements CountryService {
-
 
     @Autowired
     CountryRepository countryRepository;
     @Override
     public CountryEntity add(CountryEntity countryEntity) {
         return countryRepository.save(countryEntity);
+    }
+
+    @Override
+    public CountryEntity update(Long id, CountryEntity countryEntity) {
+        CountryEntity result = findById(id);
+        if (result != null) {
+            result.setCountryName(countryEntity.getCountryName());
+            result.setCountryCode(countryEntity.getCountryCode());
+            result.setImageUrl(countryEntity.getImageUrl());
+            result.setDescription(countryEntity.getDescription());
+            return countryRepository.save(result);
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        CountryEntity result = findById(id);
+        if (result != null) {
+            countryRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<CountryEntity> findAll() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public CountryEntity findById(Long id) {
+        Optional<CountryEntity> result = countryRepository.findById(id);
+        return result.orElse(null);
     }
 
     ObjectMapper mapper = new ObjectMapper();
