e5ae249bc4b7a2f32b511f6f680a9a83c970d694
mfyasykur <mf.yasykur@gmail.com>
2022-11-26 02:47:26 +0700

    Add Swagger-Open API configuration

---
+++ src/main/java/org/binar/eflightticket_b2/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.binar.eflightticket_b2.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("B2 E-Flight Reservation REST API")
+                        .description("Under production :: not release yet")
+                );
+    }
+}
