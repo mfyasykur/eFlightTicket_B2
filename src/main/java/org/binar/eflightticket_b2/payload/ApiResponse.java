8af6c149e9b08515e07671ee31d17460627b723e
haris <haris.aulia@gmail.com>
2022-11-25 10:39:59 +0700

    added standardized API Response

---
+++ src/main/java/org/binar/eflightticket_b2/payload/ApiResponse.java
@@ -0,0 +1,47 @@
+package org.binar.eflightticket_b2.payload;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@JsonPropertyOrder({
+        "success",
+        "message"
+})
+public class ApiResponse {
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonIgnore
+    private HttpStatus httpStatus;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private transient Object data;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public ApiResponse(Boolean success, String message, Object object) {
+        this.success = success;
+        this.message = message;
+        this.data = object;
+    }
+
+    public ApiResponse(Boolean success, String message, HttpStatus httpStatus) {
+        this.success = success;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
