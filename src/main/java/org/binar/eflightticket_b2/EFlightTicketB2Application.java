562aba2039ce1f1433d72cd5e4e5ca6ac90f51d4
haris <haris.aulia@gmail.com>
2022-11-26 21:19:27 +0700

    added annotations

--- src/main/java/org/binar/eflightticket_b2/EFlightTicketB2Application.java
+++ src/main/java/org/binar/eflightticket_b2/EFlightTicketB2Application.java
@@ -2,8 +2,12 @@ package org.binar.eflightticket_b2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "org.binar.eflightticket_b2.repository")
 public class EFlightTicketB2Application {
 
     public static void main(String[] args) {
