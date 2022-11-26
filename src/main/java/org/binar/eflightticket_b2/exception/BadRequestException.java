56b9e2e57f9497deb2da9944f26b6a5439a84c54
haris <haris.aulia@gmail.com>
2022-11-26 21:20:50 +0700

    added BadRequestException

---
+++ src/main/java/org/binar/eflightticket_b2/exception/BadRequestException.java
@@ -0,0 +1,24 @@
+package org.binar.eflightticket_b2.exception;
+
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ResponseStatus(BAD_REQUEST)
+public class BadRequestException extends RuntimeException{
+
+    private final Map<String, String> errorMessage;
+
+    public BadRequestException(Map<String, String> errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public Map<String, String> getErrorMessage() {
+        return errorMessage;
+    }
+    
+}
