99fb11f88eb16ecdd38afe1b55f16607aabe7810
haris <haris.aulia@gmail.com>
2022-11-29 08:41:22 +0700

    added model mapper bean

--- src/main/java/org/binar/eflightticket_b2/EFlightTicketB2Application.java
+++ src/main/java/org/binar/eflightticket_b2/EFlightTicketB2Application.java
@@ -1,7 +1,9 @@
 package org.binar.eflightticket_b2;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,5 +15,12 @@ public class EFlightTicketB2Application {
     public static void main(String[] args) {
         SpringApplication.run(EFlightTicketB2Application.class, args);
     }
+
+
+    @Bean
+    ModelMapper modelMapper (){
+        return new ModelMapper();
+    }
+
 
 }
