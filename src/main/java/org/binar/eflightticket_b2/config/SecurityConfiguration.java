5d64a8ae64f38545a1858327c2bb7daf737a926a
haris <haris.aulia@gmail.com>
2022-12-02 14:25:42 +0700

    created SecurityConfiguration class

---
+++ src/main/java/org/binar/eflightticket_b2/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package org.binar.eflightticket_b2.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
