a8ea341785f22fd068a085ba58628cbf7983b5b8
haris <haris.aulia@gmail.com>
2022-12-03 15:07:02 +0700

    moved the endpoint to Auth controller

--- src/main/java/org/binar/eflightticket_b2/controller/UserController.java
+++ src/main/java/org/binar/eflightticket_b2/controller/UserController.java
@@ -27,16 +27,6 @@ public class UserController {
     private final UserService userService;
     private final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping("/signup")
-    public ResponseEntity<ApiResponse> addUser(@Valid @RequestBody UsersDTO user){
-        Users users = userService.mapToEntity(user);
-        Users savedUser = userService.addUser(users);
-        ApiResponse apiResponse = new ApiResponse(
-                Boolean.TRUE, "Successfully added user data with id : " +savedUser.getId());
-        log.info("successfully added user data");
-        return new ResponseEntity<>(apiResponse, CREATED);
-    }
-
     @DeleteMapping("/delete/{username}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable ("username") @NotBlank String username){
         Users deletedUser = userService.deleteUser(username);
