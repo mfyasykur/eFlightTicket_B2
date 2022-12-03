6e1ce8f566ffdecb79829038669df008426d0c7e
haris <haris.aulia@gmail.com>
2022-12-03 11:40:37 +0700

    added using standardized APIError Response

--- src/main/java/org/binar/eflightticket_b2/filter/AuthorizationFilter.java
+++ src/main/java/org/binar/eflightticket_b2/filter/AuthorizationFilter.java
@@ -6,7 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.binar.eflightticket_b2.exception.ResourceNotFoundException;
+import org.binar.eflightticket_b2.exception.APIError;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,17 +17,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class AuthorizationFilter extends OncePerRequestFilter {
+
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
 
         if (request.getServletPath().equals("/users/login")){
             filterChain.doFilter(request,response);
@@ -42,17 +43,29 @@ public class AuthorizationFilter extends OncePerRequestFilter {
                     String username = decodedJWT.getSubject();
                     String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
                     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                    Arrays.stream(roles).forEach(role -> {
-                        authorities.add(new SimpleGrantedAuthority(role));
-                    });
+                    Arrays.stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
                     UsernamePasswordAuthenticationToken authenticationToken =
                             new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     filterChain.doFilter(request,response);
                 }catch (Exception exception) {
+                    HashMap<String, String> message = new HashMap<>();
+                    message.put("error", exception.getMessage());
+                    APIError apiError = APIError.builder().
+                            timestamp(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date()))
+                            .status(FORBIDDEN.value())
+                            .error(FORBIDDEN.name())
+                            .message(message)
+                            .path(request.getRequestURI())
+                            .request(request.getMethod())
+                            .description("check for valid authorities")
+                            .build();
                     response.setContentType(APPLICATION_JSON_VALUE);
-                    logger.info(exception.getLocalizedMessage());
+                    logger.info(exception.getMessage());
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    objectMapper.registerModule(new JavaTimeModule());
                     response.setStatus(FORBIDDEN.value());
+                    objectMapper.writeValue(response.getOutputStream(), apiError);
                 }
             }else {
                 filterChain.doFilter(request, response);
