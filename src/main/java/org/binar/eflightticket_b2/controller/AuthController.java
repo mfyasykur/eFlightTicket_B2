c4826493c4ecc8e2e003ae8bfdd5a95a03fbca6d
haris <haris.aulia@gmail.com>
2022-12-03 11:44:09 +0700

    added login endpoint

--- src/main/java/org/binar/eflightticket_b2/controller/AuthController.java
+++ src/main/java/org/binar/eflightticket_b2/controller/AuthController.java
@@ -1,7 +1,6 @@
 package org.binar.eflightticket_b2.controller;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.binar.eflightticket_b2.dto.LoginRequest;
 import org.binar.eflightticket_b2.payload.ApiResponse;
@@ -13,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -37,37 +35,36 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletRequest request) {
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
-        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
+            Authentication authenticate = authenticationManager.authenticate(authenticationToken);
 
+            User user =(User) authenticate.getPrincipal();
 
-        User user =(User) authenticate.getPrincipal();
+            Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+            String accessToken = JWT.create()
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 7 * 60 * 1000))
+                    .withIssuer(request.getRequestURI())
+                    .withClaim("roles", user.getAuthorities()
+                            .stream()
+                            .map(GrantedAuthority::getAuthority).toList())
+                    .sign(algorithm);
+            log.info("Info :  successfully generated access token user");
+            String refreshToken = JWT.create()
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 70 * 60 * 1000))
+                    .withIssuer(request.getRequestURL().toString())
+                    .sign(algorithm);
+            log.info("Info :  successfully generated refresh token user");
+            HashMap<String, String> tokens = new HashMap<>();
+            tokens.put("access_token", accessToken);
+            tokens.put("refresh_token", refreshToken);
+            ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, "Successfully Login", tokens);
+            return ResponseEntity.ok()
+                    .header("access_token", accessToken)
+                    .body(apiResponse);
 
-        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-        String accessToken = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 7 * 60 * 1000))
-                .withIssuer(request.getRequestURI())
-                .withClaim("roles", user.getAuthorities()
-                        .stream()
-                        .map(GrantedAuthority::getAuthority).toList())
-                .sign(algorithm);
-        log.info("Info :  successfully generated access token user");
-        String refreshToken = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 70 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .sign(algorithm);
-        log.info("Info :  successfully generated refresh token user");
-        HashMap<String, String> tokens = new HashMap<>();
-        tokens.put("access_token", accessToken);
-        tokens.put("refresh_token", refreshToken);
-        ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, "Successfully Login", tokens);
-
-        return ResponseEntity.ok()
-                .header("access_token", accessToken)
-                .body(apiResponse);
     }
 
 }
