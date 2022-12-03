d7956368ff5123ba68b81ec1dc3597e206edced7
haris <haris.aulia@gmail.com>
2022-12-03 14:41:00 +0700

    added refresh token endpoint

--- src/main/java/org/binar/eflightticket_b2/controller/AuthController.java
+++ src/main/java/org/binar/eflightticket_b2/controller/AuthController.java
@@ -1,8 +1,13 @@
 package org.binar.eflightticket_b2.controller;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.binar.eflightticket_b2.dto.LoginRequest;
+import org.binar.eflightticket_b2.entity.Role;
+import org.binar.eflightticket_b2.entity.Users;
+import org.binar.eflightticket_b2.exception.BadRequestException;
 import org.binar.eflightticket_b2.payload.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,10 +20,15 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
+import org.binar.eflightticket_b2.service.UserService;
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @RestController
@@ -29,8 +39,12 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
 
-    public AuthController(AuthenticationManager authenticationManager) {
+    private final UserService userService;
+
+
+    public AuthController(AuthenticationManager authenticationManager, UserService userService) {
         this.authenticationManager = authenticationManager;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -65,6 +79,45 @@ public class AuthController {
                     .header("access_token", accessToken)
                     .body(apiResponse);
 
+    }
+
+
+    @GetMapping(value = "/refresh")
+    public ResponseEntity<ApiResponse> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        String authHeader = request.getHeader(AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            try {
+                String refreshToken = authHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refreshToken);
+                String username = decodedJWT.getSubject();
+                Users userByUsername = userService.getUserByUsername(username);
+
+                String accessToken = JWT.create()
+                        .withSubject(userByUsername.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 1 * 60 * 1000))
+                        .withIssuer(request.getRequestURI())
+                        .withClaim("roles", userByUsername.getRoles().stream().map(Role::getName).toList())
+                        .sign(algorithm);
+                HashMap<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, "Successfully created new access token",tokens);
+                return ResponseEntity.ok()
+                        .body(apiResponse);
+
+            }catch (Exception exception){
+                HashMap<String, String> errorMessage = new HashMap<>();
+                errorMessage.put("error", exception.getMessage());
+                throw new BadRequestException(errorMessage);
+            }
+        }else {
+            HashMap<String, String> errorMessage = new HashMap<>();
+            errorMessage.put("error", "refresh token is missing or not with valid form");
+            throw new BadRequestException(errorMessage);
+        }
     }
 
 }
