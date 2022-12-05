bf17f12b02bc8852bc0fe616a242fda60bb9a511
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2022-12-05 18:33:23 +0700

    - Update Anotasi Country

--- src/main/java/org/binar/eflightticket_b2/dto/CountryDTO.java
+++ src/main/java/org/binar/eflightticket_b2/dto/CountryDTO.java
@@ -1,14 +1,16 @@
 package org.binar.eflightticket_b2.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class CountryDTO{
     String countryName;
     String countryCode;
