6fd7625a4e968543d6850abbbb82fae5c09fd2a3
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2022-12-05 18:32:48 +0700

    - Update Anotasi City

--- src/main/java/org/binar/eflightticket_b2/dto/CityDTO.java
+++ src/main/java/org/binar/eflightticket_b2/dto/CityDTO.java
@@ -1,5 +1,7 @@
 package org.binar.eflightticket_b2.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,11 +10,12 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class CityDTO{
     String cityName;
     String cityCode;
     String imageUrl;
     String description;
-    List<AirportDTO> airportEntities;
 }
