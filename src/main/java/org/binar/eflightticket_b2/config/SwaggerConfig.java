fca77ef25301699e4749b40668d413b092df3fbb
mfyasykur <mf.yasykur@gmail.com>
2022-12-09 13:53:07 +0700

    add CORS Config

--- src/main/java/org/binar/eflightticket_b2/config/SwaggerConfig.java
+++ src/main/java/org/binar/eflightticket_b2/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package org.binar.eflightticket_b2.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("B2 E-Flight Ticket Reservation REST API")
                         .description("Under production :: not release yet")
-                );
+                )
+                .servers(List.of(new Server().url("https://eflightticketb2-staging.up.railway.app/")));
     }
 }
