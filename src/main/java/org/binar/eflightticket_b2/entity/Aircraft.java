bf92b6561bed1a58aa3f99a89d30023bdec465cc
mfyasykur <mf.yasykur@gmail.com>
2022-12-11 01:31:30 +0700

    Update Aircraft Entity

--- src/main/java/org/binar/eflightticket_b2/entity/Aircraft.java
+++ src/main/java/org/binar/eflightticket_b2/entity/Aircraft.java
@@ -1,11 +1,9 @@
 package org.binar.eflightticket_b2.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -14,18 +12,6 @@ import java.time.LocalDateTime;
 @Table(name = "Aircraft")
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt", "flightDetail"}, allowGetters = true)
 public class Aircraft extends BaseEntity {
-
-//    @Builder
-//    public Aircraft(String manufacture, String manufactureCode, String registerCode, Integer seatCapacity, Integer baggageCapacity, SizeType sizeType, FlightDetail flightDetail, Long id, LocalDateTime createdAt, LocalDateTime updatedAt) {
-//        super(id, createdAt, updatedAt);
-//        this.manufacture = manufacture;
-//        this.manufactureCode = manufactureCode;
-//        this.registerCode = registerCode;
-//        this.seatCapacity = seatCapacity;
-//        this.baggageCapacity = baggageCapacity;
-//        this.sizeType = sizeType;
-//        this.flightDetail = flightDetail;
-//    }
 
     private String manufacture;
 
@@ -46,7 +32,7 @@ public class Aircraft extends BaseEntity {
     @Enumerated
     private SizeType sizeType;
 
-    @OneToOne(mappedBy = "aircraftDetail", cascade = CascadeType.ALL)
-    @JsonIgnore
-    private FlightDetail flightDetail;
+//    @OneToOne(mappedBy = "aircraftDetail", cascade = CascadeType.ALL)
+//    @JsonIgnore
+//    private FlightDetail flightDetail;
 }
