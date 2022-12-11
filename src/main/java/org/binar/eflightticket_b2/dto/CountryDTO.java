c5977ad6af71f89b48f3bf9f8238d644e7107582
mfyasykur <mf.yasykur@gmail.com>
2022-12-11 21:06:43 +0700

    Add JsonIgnoreProperties for "id"

--- src/main/java/org/binar/eflightticket_b2/dto/CountryDTO.java
+++ src/main/java/org/binar/eflightticket_b2/dto/CountryDTO.java
@@ -1,20 +1,20 @@
 package org.binar.eflightticket_b2.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = {"id"}, allowGetters = true)
 public class CountryDTO{
+    Long id;
     String countryName;
     String countryCode;
     String imageUrl;
     String description;
-//    List<CityDTO> cityEntities;
 }
