30a30c58a8c32fb1e35613da74ce9f1b68c3d954
mfyasykur <mf.yasykur@gmail.com>
2022-12-11 21:02:50 +0700

    Remove unused JsonIgnoreProperties for value "createdAt", "updatedAt", "id"

--- src/main/java/org/binar/eflightticket_b2/entity/City.java
+++ src/main/java/org/binar/eflightticket_b2/entity/City.java
@@ -1,18 +1,15 @@
 package org.binar.eflightticket_b2.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
-@JsonIgnoreProperties(value = {"createdAt", "updatedAt", "airportDetail"}, allowGetters = true)
 public class City extends BaseEntity{
 
     @Column(name = "city_name")
