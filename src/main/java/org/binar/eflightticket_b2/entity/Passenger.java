b7a29980a6c1a77198bb8ab12837e71d3493dbb5
haris <haris.aulia@gmail.com>
2022-12-16 14:00:22 +0700

    updated Passenger

--- src/main/java/org/binar/eflightticket_b2/entity/Passenger.java
+++ src/main/java/org/binar/eflightticket_b2/entity/Passenger.java
@@ -3,17 +3,25 @@ package org.binar.eflightticket_b2.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Passenger extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private GENDER gender;
     private Long age;
+    @Column(name = "age_category")
     private AGE ageCategory;
+
+    @ManyToOne
+    private Booking booking;
 
 }
