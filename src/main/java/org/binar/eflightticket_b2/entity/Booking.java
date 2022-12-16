a9a1448645823ddd57f86bc040b2c4a7ca37f98d
haris <haris.aulia@gmail.com>
2022-12-16 13:57:35 +0700

    added Booking entity

---
+++ src/main/java/org/binar/eflightticket_b2/entity/Booking.java
@@ -0,0 +1,51 @@
+package org.binar.eflightticket_b2.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends BaseEntity {
+
+    @ManyToOne
+    @JsonIgnore
+    private Users users;
+
+    @ManyToOne
+    private Schedule schedule;
+
+    @OneToMany
+    @Column(name = "passenger")
+    @JsonIgnore
+    private List<Passenger> passengersList = new ArrayList<>();
+
+    @Column(name = "is_success")
+    private Boolean isSuccess;
+
+    @Column(name = "due_valid")
+    private LocalDateTime dueValid;
+
+    @Column(name = "is_valid")
+    private Boolean isValid;
+
+    @Column(name = "payement_method")
+    private String paymentMethod;
+
+    @Column(name = "booking_code")
+    private String bookingCode;
+
+
+
+}
