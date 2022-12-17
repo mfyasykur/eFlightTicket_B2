335fef603c7be62db1e256f98e1a4ed29e15125c
haris <haris.aulia@gmail.com>
2022-12-17 21:02:07 +0700

    defined payment method

--- src/main/java/org/binar/eflightticket_b2/service/BookingService.java
+++ src/main/java/org/binar/eflightticket_b2/service/BookingService.java
@@ -1,10 +1,13 @@
 package org.binar.eflightticket_b2.service;
 
 import org.binar.eflightticket_b2.dto.BookingRequest;
+import org.binar.eflightticket_b2.dto.PaymentDTO;
 import org.binar.eflightticket_b2.entity.Booking;
+
 
 public interface BookingService {
 
     Booking addBooking(BookingRequest bookingRequest);
+    Booking payment(PaymentDTO paymentDTO);
 
 }
