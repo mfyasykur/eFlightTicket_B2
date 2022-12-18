020819a67ab10204fc8e419c836c6bcd95ba341d
haris <haris.aulia@gmail.com>
2022-12-18 11:02:44 +0700

    defined successBookingHistory method

--- src/main/java/org/binar/eflightticket_b2/service/BookingService.java
+++ src/main/java/org/binar/eflightticket_b2/service/BookingService.java
@@ -4,10 +4,13 @@ import org.binar.eflightticket_b2.dto.BookingRequest;
 import org.binar.eflightticket_b2.dto.PaymentDTO;
 import org.binar.eflightticket_b2.entity.Booking;
 
+import java.util.List;
+
 
 public interface BookingService {
 
     Booking addBooking(BookingRequest bookingRequest);
     Booking payment(PaymentDTO paymentDTO);
+    List<Booking> successBookingHistory(Long userId, Boolean isSuccess);
 
 }
