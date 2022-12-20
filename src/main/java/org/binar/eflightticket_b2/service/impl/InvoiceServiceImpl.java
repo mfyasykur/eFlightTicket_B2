d02cae1dc07099176089fc3049206b245e823e09
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2022-12-20 18:31:21 +0700

    Update Invoice Service implement

--- src/main/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImpl.java
+++ src/main/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImpl.java
@@ -2,8 +2,10 @@ package org.binar.eflightticket_b2.service.impl;
 
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import org.binar.eflightticket_b2.entity.Invoice;
-import org.binar.eflightticket_b2.repository.InvoiceRepository;
+import org.binar.eflightticket_b2.entity.Booking;
+import org.binar.eflightticket_b2.entity.Passenger;
+import org.binar.eflightticket_b2.exception.ResourceNotFoundException;
+import org.binar.eflightticket_b2.repository.BookingRepository;
 import org.binar.eflightticket_b2.service.InvoiceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,31 +13,89 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
 
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
     private final static Logger log =  LoggerFactory.getLogger(InvoiceServiceImpl.class);
+    
     @Autowired
-    private InvoiceRepository invoiceRepository;
+    private BookingRepository bookingRepository;
 
     @Override
-    public byte[] generateInvoice(Long bookingId) throws FileNotFoundException, JRException {
+    public JasperPrint generateInvoice(Long bookingId) throws FileNotFoundException, JRException {
 
-        log.info("Generating pdf invoice for id order {}", bookingId);
-
-        List<Invoice> invoices = invoiceRepository.get(bookingId);
-        File design = ResourceUtils.getFile("classpath:jasper/ETicket.jrxml");
-        JasperReport report = JasperCompileManager.compileReport(design.getAbsolutePath());
-
-        Map<String, Object> map = new HashMap<>();
-        JasperPrint jasperPrint = JasperFillManager.fillReport(report, map, new JRBeanCollectionDataSource(invoices));
-
-        return JasperExportManager.exportReportToPdf(jasperPrint);
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> {
+                    log.error("ERROR : Report data not found");
+                    return new ResourceNotFoundException("Booking", "id", bookingId);
+                });
+        Map<String, Object> dataMap = dataParameter(booking);
+        List<Booking> bookingsCollect = new LinkedList<>();
+        bookingsCollect.add(booking);
+        dataMap.put("userData", new JRBeanCollectionDataSource(bookingsCollect));
+        JasperPrint jasperPrint = JasperFillManager.fillReport(
+                JasperCompileManager.compileReport(
+                        ResourceUtils.getFile("ETicket.jrxml")
+                                .getAbsolutePath())
+                , dataMap
+                , new JREmptyDataSource()
+        );
+        log.info("Info :  Has successfully created ETicket!");
+        return jasperPrint;
     }
 
+    public Map<String, Object> dataParameter(Booking bookings){
+        Long bookingId = bookings.getId();
+        LocalDate departureDate = bookings.getSchedule().getDepartureDate();
+        LocalDate arrivalDate = bookings.getSchedule().getArrivalDate();
+        LocalTime departureTime = bookings.getSchedule().getDepartureTime();
+        LocalTime arrivalTime = bookings.getSchedule().getArrivalTime();
+        String departureCityName = bookings.getSchedule().getFlightDetail().getDeparture().getCityDetails().getCityName();
+        String departureCountryName = bookings.getSchedule().getFlightDetail().getDeparture().getCountryDetails().getCountryName();
+        String departureAirportName = bookings.getSchedule().getFlightDetail().getDeparture().getAirportDetails().getAirportName();
+        String departureAirportCode = bookings.getSchedule().getFlightDetail().getDeparture().getAirportDetails().getAirportCode();
+        String arrivalCityName = bookings.getSchedule().getFlightDetail().getArrival().getCityDetails().getCityName();
+        String arrivalCountryName = bookings.getSchedule().getFlightDetail().getArrival().getCountryDetails().getCountryName();
+        String arrivalAirportName = bookings.getSchedule().getFlightDetail().getArrival().getAirportDetails().getAirportName();
+        String arrivalAirportCode = bookings.getSchedule().getFlightDetail().getArrival().getAirportDetails().getAirportCode();
+
+        for (int i = 0; i < dataParameter(bookings).size(); i++) {
+            List<Passenger> passengerId = bookings.getPassengersList();
+            List<Passenger> gender = bookings.getPassengersList();
+            List<Passenger> firstName = bookings.getPassengersList();
+            List<Passenger> lastName = bookings.getPassengersList();
+            List<Passenger> age = bookings.getPassengersList();
+            List<Passenger> ageCategory = bookings.getPassengersList();
+        }
+
+
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("bookingId", bookingId);
+        dataMap.put("departureDate", departureDate);
+        dataMap.put("arrivalDate", arrivalDate);
+        dataMap.put("departureTime", departureTime);
+        dataMap.put("arrivalTime", arrivalTime);
+        dataMap.put("departureCityName", departureCityName);
+        dataMap.put("departureCountryName", departureCountryName);
+        dataMap.put("departureAirportName", departureAirportName);
+        dataMap.put("departureAirportCode", departureAirportCode);
+        dataMap.put("arrivalCityName", arrivalCityName);
+        dataMap.put("arrivalCountryName", arrivalCountryName);
+        dataMap.put("arrivalAirportName", arrivalAirportName);
+        dataMap.put("arrivalAirportCode", arrivalAirportCode);
+
+        dataMap.put("passengerId", passengerId);
+        dataMap.put("gender", gender);
+        dataMap.put("firstName", firstName);
+        dataMap.put("lastName", lastName);
+        dataMap.put("age", age);
+        dataMap.put("ageCategory", ageCategory);
+
+        log.info("Info :  mapping data from database success");
+        return dataMap;
+    }
 }
