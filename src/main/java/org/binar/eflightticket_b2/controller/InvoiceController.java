098fe950272841dae3588a03b0b9f2dc33f77fe0
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2022-12-20 18:30:28 +0700

    Update Invoice Controller class

--- src/main/java/org/binar/eflightticket_b2/controller/InvoiceController.java
+++ src/main/java/org/binar/eflightticket_b2/controller/InvoiceController.java
@@ -1,16 +1,23 @@
 package org.binar.eflightticket_b2.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.binar.eflightticket_b2.service.InvoiceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 @RequestMapping("/invoice")
@@ -22,12 +29,15 @@ public class InvoiceController {
     private InvoiceService invoiceService;
 
 
-    @GetMapping("/generatePdf{id}")
-    public ResponseEntity<byte[]> generateInvoice(@PathVariable("id") Long bookingId) throws JRException, FileNotFoundException {
-        byte[] data = invoiceService.generateInvoice(bookingId);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline;filename= invoice.pdf");
-        log.info(" Invoice generated ");
-        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+    @GetMapping("/generatePdf")
+    public ResponseEntity<ApiResponse> getInvoicePrint(HttpServletResponse response, @RequestParam(name = "bookingId") Long bookingId) throws IOException, JRException {
+        JasperPrint jasperPrint = invoiceService.generateInvoice(bookingId);
+        response.setContentType("application/x-download");
+        response.addHeader("Content-disposition", "attachment; filename=invoice_report.pdf");
+        OutputStream out = response.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint,out);
+        ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, "success", jasperPrint);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
+
 }
