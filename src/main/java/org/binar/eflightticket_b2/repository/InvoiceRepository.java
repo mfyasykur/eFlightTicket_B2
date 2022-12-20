138c78f1fdf8e90e60782e5903f3fe18476d9d11
Miftahul Huda Guntara <hudalgtr99@gmail.com>
2022-12-20 18:30:44 +0700

    Delete Invoice repository

--- src/main/java/org/binar/eflightticket_b2/repository/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package org.binar.eflightticket_b2.repository;
-
-import org.binar.eflightticket_b2.entity.Invoice;
-import java.util.List;
-
-public interface InvoiceRepository {
-    List<Invoice> get(Long bookingId);
-}
