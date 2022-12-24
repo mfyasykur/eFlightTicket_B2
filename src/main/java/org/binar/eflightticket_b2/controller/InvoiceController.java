d61e9cb032071350ab4ebb8c2119f567407eeeeb
mfyasykur <mf.yasykur@gmail.com>
2022-12-24 19:37:04 +0700

    Add QRCode Generator Controller

--- src/main/java/org/binar/eflightticket_b2/controller/InvoiceController.java
+++ src/main/java/org/binar/eflightticket_b2/controller/InvoiceController.java
@@ -1,13 +1,17 @@
 package org.binar.eflightticket_b2.controller;
 
+import com.google.zxing.WriterException;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import org.binar.eflightticket_b2.payload.ApiResponse;
+import org.binar.eflightticket_b2.service.BookingService;
 import org.binar.eflightticket_b2.service.InvoiceService;
+import org.binar.eflightticket_b2.service.impl.InvoiceServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/invoice")
@@ -24,7 +29,6 @@ public class InvoiceController {
 
     @Autowired
     private InvoiceService invoiceService;
-
 
     @GetMapping("/showticket")
     public ResponseEntity<org.binar.eflightticket_b2.payload.ApiResponse> getEticket(HttpServletResponse response, @RequestParam(name = "bookingId") Long bookingId) throws IOException, JRException {
@@ -44,6 +48,18 @@ public class InvoiceController {
         response.addHeader("Content-disposition", "attachment; filename=eticket"+bookingId+".pdf");
         OutputStream out = response.getOutputStream();
         JasperExportManager.exportReportToPdfStream(jasperPrint,out);
+    }
+
+    @GetMapping("/showQRCode")
+    public ResponseEntity<ApiResponse> generateQRCode(@RequestParam(name = "bookingId") Long bookingId) throws WriterException, IOException {
+
+        byte[] image;
+        image = invoiceService.getQRCodeImage(bookingId, 250, 250);
+        String qrCode = Base64.getEncoder().encodeToString(image);
+
+        ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, "success", qrCode);
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=invoiceQRCode.PNG").body(apiResponse);
     }
 
 }
