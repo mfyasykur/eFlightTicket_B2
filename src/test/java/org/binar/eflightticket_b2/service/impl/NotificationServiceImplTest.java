8a4a685c1666a16478eaf95418b9df6f683a404d
Derr_Dev <haris.aulia404@gmail.com>
2022-12-30 17:23:06 +0700

    added NotificationServiceImplTest

---
+++ src/test/java/org/binar/eflightticket_b2/service/impl/NotificationServiceImplTest.java
@@ -0,0 +1,16 @@
+package org.binar.eflightticket_b2.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotificationServiceImplTest {
+
+    @Test
+    void getAllNotification() {
+    }
+
+    @Test
+    void addNotification() {
+    }
+}
