de92994cd0e9b39eb7a86013f9395b65b5371f51
Derr_Dev <haris.aulia404@gmail.com>
2022-12-30 17:24:35 +0700

    added all dependencies

--- src/test/java/org/binar/eflightticket_b2/service/impl/NotificationServiceImplTest.java
+++ src/test/java/org/binar/eflightticket_b2/service/impl/NotificationServiceImplTest.java
@@ -1,10 +1,21 @@
 package org.binar.eflightticket_b2.service.impl;
 
+import org.binar.eflightticket_b2.repository.NotificationRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class NotificationServiceImplTest {
+    @Mock
+    NotificationRepository notificationRepository;
+
+    @InjectMocks
+    NotificationServiceImpl notificationService;
 
     @Test
     void getAllNotification() {
