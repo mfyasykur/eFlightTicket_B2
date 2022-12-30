c7b69f474051a050698278a676ba39ab29ded316
Derr_Dev <haris.aulia404@gmail.com>
2022-12-30 15:53:18 +0700

    Added BookingServiceImplTest

---
+++ src/test/java/org/binar/eflightticket_b2/service/impl/BookingServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.binar.eflightticket_b2.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceImplTest {
+
+    @Test
+    void addBooking() {
+    }
+
+    @Test
+    void payment() {
+    }
+
+    @Test
+    void successBookingHistory() {
+    }
+
+    @Test
+    void getAllBookingHistory() {
+    }
+
+    @Test
+    void getBookingHistory() {
+    }
+}
