dc5b2b383cd88a1ec1b980dace88a18e2be004c0
Derr_Dev <haris.aulia404@gmail.com>
2022-12-30 16:25:11 +0700

    Added success case on addBookin in BookingServiceImplTest

--- src/test/java/org/binar/eflightticket_b2/service/impl/BookingServiceImplTest.java
+++ src/test/java/org/binar/eflightticket_b2/service/impl/BookingServiceImplTest.java
@@ -1,13 +1,104 @@
 package org.binar.eflightticket_b2.service.impl;
 
+import org.binar.eflightticket_b2.dto.BookingRequest;
+import org.binar.eflightticket_b2.dto.PassengerRequest;
+import org.binar.eflightticket_b2.entity.Booking;
+import org.binar.eflightticket_b2.entity.Passenger;
+import org.binar.eflightticket_b2.entity.Schedule;
+import org.binar.eflightticket_b2.entity.Users;
+import org.binar.eflightticket_b2.repository.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.BDDMockito.*;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class BookingServiceImplTest {
 
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    BookingRepository bookingRepository;
+
+    @Mock
+    ScheduleRepository scheduleRepository;
+
+    @Mock
+    PassengerRepository passengerRepository;
+
+    @Mock
+    NotificationRepository notificationRepository;
+
+    @Mock
+    NotificationServiceImpl notificationService;
+
+    @InjectMocks
+    private BookingServiceImpl bookingService;
+
+    @Mock
+    private PassengerServiceImpl passengerService;
+
+    @Mock
+    private UserServiceImpl userService;
+
+    @Mock
+    private ScheduleServiceImpl scheduleService;
+
+
+    @BeforeEach
+    void setUp() {
+        this.notificationService = new NotificationServiceImpl(notificationRepository);
+    }
+
+
     @Test
-    void addBooking() {
+    void addBookingSuccess() {
+
+        PassengerRequest haris = new PassengerRequest();
+        haris.setFirstName("haris");
+        PassengerRequest aulia = new PassengerRequest();
+        haris.setFirstName("aulia");
+        List<PassengerRequest> listOfRequestPassenger = List.of(haris, aulia);
+
+        Passenger budi = new Passenger();
+        budi.setFirstName("budi");
+        Passenger ani = new Passenger();
+        ani.setFirstName("ani");
+        List<Passenger> listOfPassenger = List.of(budi, ani);
+
+        Users users = new Users();
+        users.setId(111l);
+        users.setFirstName("haris");
+        users.setLastName("aulia");
+        users.setEmail("haris.aulia@gmail.com");
+        users.setPassword("some-password");
+
+        Schedule schedule = new Schedule();
+        schedule.setId(1l);
+        schedule.setNetPrice(500_000);
+
+        BookingRequest bookingRequest = new BookingRequest();
+        bookingRequest.setScheduleId(1l);
+        bookingRequest.setUserId(1l);
+        bookingRequest.setPassengerRequests(listOfRequestPassenger);
+
+        Booking build = Booking.builder().bookingCode("some-code").build();
+
+        given(userService.getUserById(1l)).willReturn(users);
+        given(scheduleService.getScheduleById(1l)).willReturn(schedule);
+
+
+        Booking booking = bookingService.addBooking(bookingRequest);
+
+
     }
 
     @Test
