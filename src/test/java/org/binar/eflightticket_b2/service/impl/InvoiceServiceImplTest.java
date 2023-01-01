b652f391d95fd0a312357584371ff26eced58e44
Derr_Dev <haris.aulia404@gmail.com>
2023-01-02 00:50:32 +0700

    added success and failed test

--- src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
+++ src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
@@ -1,9 +1,23 @@
 package org.binar.eflightticket_b2.service.impl;
 
+import com.google.zxing.WriterException;
+import net.sf.jasperreports.engine.JRException;
+import org.assertj.core.api.Assertions;
+import org.binar.eflightticket_b2.dto.BAGGAGE;
+import org.binar.eflightticket_b2.entity.*;
+import org.binar.eflightticket_b2.exception.ResourceNotFoundException;
 import org.binar.eflightticket_b2.repository.BookingRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class InvoiceServiceImplTest {
@@ -11,5 +25,78 @@ public class InvoiceServiceImplTest {
     @Mock
     private BookingRepository bookingRepository;
 
+    @InjectMocks
+    private InvoiceServiceImpl invoiceService;
 
+    @Test
+    void jasperReportTestSuccess() throws JRException, IOException {
+        // aiport
+        Airport airport = new Airport();
+        airport.setAirportCode("CGK");
+        airport.setAirportName("Soekarno");
+
+        //city
+        City city = new City();
+        city.setCityCode("JKT");
+        city.setCityName("Jakarta");
+
+        //Country
+        Country country = new Country();
+        country.setCountryCode("ID");
+        country.setCountryName("Indonesia");
+
+
+        AirportDetail airportDetail = new AirportDetail();
+        airportDetail.setAirportDetails(airport);
+        airportDetail.setCityDetails(city);
+        airportDetail.setCountryDetails(country);
+
+        FlightDetail flightDetail = new FlightDetail();
+        flightDetail.setArrival(airportDetail);
+        flightDetail.setDeparture(airportDetail);
+        Schedule schedule = Schedule.builder().flightDetail(flightDetail).build();
+
+        Passenger passenger = new Passenger();
+        passenger.setId(1l);
+        passenger.setFirstName("Harisatul");
+        passenger.setLastName("Aulia");
+        passenger.setGender(GENDER.MR);
+        passenger.setAgeCategory(AGE.ADULT);
+        passenger.setBaggage(BAGGAGE.KG5);
+        Passenger passenger2 = new Passenger();
+        passenger.setId(2l);
+        passenger.setFirstName("Annisa");
+        passenger.setLastName("Moreska");
+        passenger.setGender(GENDER.MR);
+        passenger.setAgeCategory(AGE.ADULT);
+        passenger.setBaggage(BAGGAGE.KG5);
+        List<Passenger> listOfPassenger = List.of(passenger, passenger2);
+
+        Booking booking = new Booking();
+        booking.setBookingCode("ANAMJHAM12");
+        booking.setId(1l);
+        booking.setSchedule(schedule);
+            booking.setPassengersList(listOfPassenger);
+
+
+        when(bookingRepository.findById(anyLong())).thenReturn(Optional.of(booking));
+
+        invoiceService.generateInvoice(anyLong());
+
+    }
+
+    @Test
+    void jasperReportTestFail() throws JRException, IOException {
+
+        when(bookingRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Assertions.assertThatThrownBy(()->invoiceService.generateInvoice(anyLong()))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void generateQRCodeTestSuccess() throws WriterException, IOException {
+
+
+    }
 }
