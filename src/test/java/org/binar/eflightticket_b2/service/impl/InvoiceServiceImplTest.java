5142c7831a3fc9dd4e837452856cd326ef53aa69
mfyasykur <mf.yasykur@gmail.com>
2023-01-02 00:07:27 +0700

    initial commit

---
+++ src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
@@ -0,0 +1,10 @@
+package org.binar.eflightticket_b2.service.impl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class InvoiceServiceImplTest {
+
+
+}
