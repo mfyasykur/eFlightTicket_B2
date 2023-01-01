73ab4277e261cac2d3e7a4149b67c9501b1fe680
mfyasykur <mf.yasykur@gmail.com>
2023-01-01 15:59:06 +0700

    Fix repository bug

--- src/main/java/org/binar/eflightticket_b2/repository/CityRepository.java
+++ src/main/java/org/binar/eflightticket_b2/repository/CityRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CityRepository extends JpaRepository<City, Long> {
-    Optional<City> findCityByCode(String cityCode);
+    Optional<City> findByCityCode(String cityCode);
 }
