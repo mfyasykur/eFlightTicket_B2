651d46ce42192c67a6b8823bd8b9e395af584d9a
Derr_Dev <haris.aulia404@gmail.com>
2023-01-02 00:14:27 +0700

    added dependency

--- src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
+++ src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
@@ -1,10 +1,15 @@
 package org.binar.eflightticket_b2.service.impl;
 
+import org.binar.eflightticket_b2.repository.BookingRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class InvoiceServiceImplTest {
+
+    @Mock
+    private BookingRepository bookingRepository;
 
 
 }
