54cb5a26d7b72cce700930c9c824e6b6d181bf4d
Derr_Dev <haris.aulia404@gmail.com>
2023-01-01 16:25:51 +0700

    rename the method

--- src/test/java/org/binar/eflightticket_b2/service/impl/AirportServiceImplTest.java
+++ src/test/java/org/binar/eflightticket_b2/service/impl/AirportServiceImplTest.java
@@ -102,10 +102,10 @@ public class AirportServiceImplTest {
         airport.setId(1L);
         airport.setAirportName("Soekarno - Hatta");
         airport.setAirportCode("SKH");
-        given(airportRepository.findAirportByCode(airport.getAirportCode())).willReturn(Optional.of(airport));
+        given(airportRepository.findByAirportCode(airport.getAirportCode())).willReturn(Optional.of(airport));
 
         airportService.findByAirportCode(airport.getAirportCode());
-        Mockito.verify(airportRepository).findAirportByCode(airport.getAirportCode());
+        Mockito.verify(airportRepository).findByAirportCode(airport.getAirportCode());
 
         var actualValue = airportService.findByAirportCode("SKH");
         Assertions.assertEquals(1L, actualValue.getId());
