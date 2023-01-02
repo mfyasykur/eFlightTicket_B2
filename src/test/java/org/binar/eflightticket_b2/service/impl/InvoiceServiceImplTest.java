3491469c069b26ee49f94a594110734bad4b4d87
mfyasykur <mf.yasykur@gmail.com>
2023-01-02 14:02:29 +0700

    Unit Testing: 100% Coverage

--- src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
+++ src/test/java/org/binar/eflightticket_b2/service/impl/InvoiceServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class InvoiceServiceImplTest {
+class InvoiceServiceImplTest {
 
     @Mock
     private BookingRepository bookingRepository;
@@ -30,7 +30,7 @@ public class InvoiceServiceImplTest {
 
     @Test
     void jasperReportTestSuccess() throws JRException, IOException {
-        // aiport
+        // airport
         Airport airport = new Airport();
         airport.setAirportCode("CGK");
         airport.setAirportName("Soekarno");
@@ -60,14 +60,14 @@ public class InvoiceServiceImplTest {
         GENDER mr = GENDER.MR;
 
         Passenger passenger = new Passenger();
-        passenger.setId(1l);
+        passenger.setId(1L);
         passenger.setFirstName("Harisatul");
         passenger.setLastName("Aulia");
         passenger.setGender(mr);
         passenger.setAgeCategory(AGE.ADULT);
         passenger.setBaggage(BAGGAGE.KG5);
         Passenger passenger2 = new Passenger();
-        passenger2.setId(2l);
+        passenger2.setId(2L);
         passenger2.setFirstName("Annisa");
         passenger2.setLastName("Moreska");
         passenger2.setGender(mrs);
@@ -77,7 +77,7 @@ public class InvoiceServiceImplTest {
 
         Booking booking = new Booking();
         booking.setBookingCode("ANAMJHAM12");
-        booking.setId(1l);
+        booking.setId(1L);
         booking.setSchedule(schedule);
         booking.setPassengersList(listOfPassenger);
 
@@ -86,10 +86,11 @@ public class InvoiceServiceImplTest {
 
         invoiceService.generateInvoice(anyLong());
 
+        verify(bookingRepository).findById(anyLong());
     }
 
     @Test
-    void jasperReportTestFail() throws JRException, IOException {
+    void jasperReportTestFail() {
 
         when(bookingRepository.findById(anyLong())).thenReturn(Optional.empty());
 
@@ -98,8 +99,72 @@ public class InvoiceServiceImplTest {
     }
 
     @Test
-    void generateQRCodeTestSuccess() throws WriterException, IOException {
+    void generateQRCodeSuccess() throws WriterException, IOException {
+
+        // airport
+        Airport airport = new Airport();
+        airport.setAirportCode("CGK");
+        airport.setAirportName("Soekarno");
+
+        //city
+        City city = new City();
+        city.setCityCode("JKT");
+        city.setCityName("Jakarta");
+
+        //Country
+        Country country = new Country();
+        country.setCountryCode("ID");
+        country.setCountryName("Indonesia");
 
 
+        AirportDetail airportDetail = new AirportDetail();
+        airportDetail.setAirportDetails(airport);
+        airportDetail.setCityDetails(city);
+        airportDetail.setCountryDetails(country);
+
+        FlightDetail flightDetail = new FlightDetail();
+        flightDetail.setArrival(airportDetail);
+        flightDetail.setDeparture(airportDetail);
+        Schedule schedule = Schedule.builder().flightDetail(flightDetail).build();
+
+        Passenger passenger = new Passenger();
+        passenger.setId(1L);
+        passenger.setFirstName("Harisatul");
+        passenger.setLastName("Aulia");
+        passenger.setGender(GENDER.MR);
+        passenger.setAgeCategory(AGE.ADULT);
+        passenger.setBaggage(BAGGAGE.KG5);
+        Passenger passenger2 = new Passenger();
+        passenger.setId(2L);
+        passenger.setFirstName("Annisa");
+        passenger.setLastName("Moreska");
+        passenger.setGender(GENDER.MR);
+        passenger.setAgeCategory(AGE.ADULT);
+        passenger.setBaggage(BAGGAGE.KG5);
+        List<Passenger> listOfPassenger = List.of(passenger, passenger2);
+
+        Booking booking = new Booking();
+        booking.setBookingCode("ANAMJHAM12");
+        booking.setId(1L);
+        booking.setSchedule(schedule);
+        booking.setPassengersList(listOfPassenger);
+
+        int width = 250;
+        int height = 250;
+
+        when(bookingRepository.findById(anyLong())).thenReturn(Optional.of(booking));
+
+        invoiceService.generateQRCodeImage(booking.getId(), width, height);
+
+        verify(bookingRepository).findById(anyLong());
+    }
+
+    @Test
+    void generateQRCodeFailed() {
+
+        when(bookingRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Assertions.assertThatThrownBy(()->invoiceService.generateQRCodeImage(1L, 0, 0))
+                .isInstanceOf(ResourceNotFoundException.class);
     }
 }
